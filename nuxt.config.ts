569a1fe2cd18719bf2aa160423c3d61e0f328ffb
dlx <xdandyx@sina.cn>
2025-11-18 11:59:10 +0800

    perf: limit node version; add defaultLocale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  ssr: true,
 
   modules: [
     "@nuxt/eslint",
     "@nuxt/icon",
     "@nuxt/image",
-    // '@nuxt/content'
     "@nuxt/test-utils",
     "@pinia/nuxt",
     "@nuxtjs/i18n",
   ],
+
+  i18n: {
+    defaultLocale: "zh-CN",
+  },
 });
