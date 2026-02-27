4bdaad872d3971986c202cc093c18347f503f4fb
dlx <xdandyx@sina.cn>
2026-02-27 17:46:40 +0800

    feat: add tauri

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import IconsResolver from "unplugin-icons/resolver";
 import ViteComponents from "unplugin-vue-components/vite";
+
+const host = process.env.TAURI_DEV_HOST || "localhost";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   ssr: false,
+  // 使开发服务器能够被其他设备发现，以便在 iOS 物理机运行。
+  devServer: { host },
   app: {
     // baseURL: ".",
   },
@@ -19,6 +24,16 @@ export default defineNuxtConfig({
         resolvers: [IconsResolver({})],
       }),
     ],
+    // 为 Tauri 命令输出提供更好的支持
+    clearScreen: false,
+    // 启用环境变量
+    // 其他环境变量可以在如下网页中获知：
+    // https://v2.tauri.app/reference/environment-variables/
+    envPrefix: ["VITE_", "TAURI_"],
+    server: {
+      // Tauri需要一个确定的端口
+      strictPort: true,
+    },
   },
   modules: [
     "@nuxt/eslint",
